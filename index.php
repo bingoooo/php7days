33ddc17f182fc76e89b612d795e65397f1ac7249
Dant Benjamin <sukkhato@gmail.com>
2015-12-13 23:23:30 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP - 7 jours pour apprendre à programmer en PHP !</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Un blog PHP statique et élégant">
+</head>
+<body>
+	<h1>Projet Reilly</h1>
+	<p>Bienvenue sur le projet Reilly, le blog minimaliste écrit en PHP.</p>
+	<?php echo "Bonjour !!! Ce texte a été généré en PHP !!!"; ?>
+	<h2><a href="post.html">Apprenez à programmer en PHP en 7 jours</a></h2>
+	<p>Avec ce numéro Hors-Série de <a href="http://www.linux-pratique.com">Linux Pratique</a>, découvrez comment programmer pour le web !</p>
+
+	<h2><a href="post.html">Préparez vos armes !</a></h2>
+	<p>Installer et configurer votre serveur web, configurez votre environnement de travail.</p>
+
+	<h2><a href="post.html">Jour 1 : À quoi ressemblera votre site ?</a></h2>
+	<p>Avant de commencer avec PHP, partez à la découverte du HTML !</p>
+
+	<h2><a href="post.html">Jour 2 : Prise en main de PHP</a></h2>
+	<p>Mettez en oeuvre PHP en gérant une authentification d'accès.</p>
+
+	<h2><a href="post.html">Jour 3 : Créer, afficher, modifier, supprimer un billet de  blog</a></h2>
+	<p>Les choses sérieuses commencent, découvrons comment gérer un contenu.</p>
+
+	<div><a href="index.php">Billets précédents</a> - <a href="index.php">Billets suivants</a></div>
+</body>
+</html>
